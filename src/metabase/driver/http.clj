ef0b2ee506ba4c63f70fb3f41c5ccf0ffea3456d
jornh <jornhansen@gmail.com>
2018-03-02 17:52:26 +0100

    shuffle mbql->native southwards to build

--- src/metabase/driver/http.clj
+++ src/metabase/driver/http.clj
@@ -31,13 +31,6 @@
                 (for [fields-path fields-paths]
                   (json-path item fields-path)))}))
 
-(defn- mbql->native
-  [query]
-  (let [table     (:source-table (:query query))
-        table-def (database->table-def (:database query) (:name table))]
-    {:query (dissoc table-def :name)
-     :mbql? true}))
-
 (defn- database->definitions
   [database]
   (json/parse-string (:definitions (:details database)) keyword))
@@ -49,6 +42,13 @@
 (defn- database->table-def
   [database name]
   (first (filter #(= (:name %) name) (database->table-defs database))))
+
+(defn- mbql->native
+  [query]
+  (let [table     (:source-table (:query query))
+        table-def (database->table-def (:database query) (:name table))]
+    {:query (dissoc table-def :name)
+     :mbql? true}))
 
 (defn- describe-database
   [database]
